50c4293209fd7c153970b3e8d04871d563bc4f78
Brandon Dykun <brandon.dykun@gmail.com>
2022-04-25 20:36:41 -0400

    edited settings.py

--- backend/css_proj/settings.py
+++ backend/css_proj/settings.py
@@ -26,7 +26,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-z^65kuv70e%2w*8%mmu#^$e3ujf5#%ny&z#y1jdo-3jsd30cq3'
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
